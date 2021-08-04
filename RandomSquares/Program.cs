5376c12785e64971aa371c816b1bbdf34dd568a6
Caseywalker <casey.walker118@gmail.com>
2021-08-04 18:53:35 -0500

    Add project files.

---
+++ RandomSquares/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandomSquares
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Random rnd = new Random();
+      var randomList = new List<int>();
+      for (var ctr = 1; ctr <= 20; ctr++)
+      {
+        randomList.Add(rnd.Next(1, 50));
+      }
+
+      foreach (var rando in randomList)
+      {
+        Console.WriteLine(rando);
+      }
+      Console.WriteLine("*********************");
+      var squaredList = randomList.Select(num => num * num).ToList();
+      squaredList.RemoveAll(numb => numb % 2 != 0);
+      foreach (var sqRnd in squaredList)
+      {
+        Console.WriteLine(sqRnd);
+      }
+
+    }
+  }
+}
